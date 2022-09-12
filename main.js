40ec5016cd6404b2dd53ca9337276302867b14d6
Lisa Jin <l.jin1@EDU.epsb.ca>
2022-09-12 14:43:20 -0600

    finalish

--- main.js
+++ main.js
@@ -3,32 +3,30 @@
 document.getElementById('btn').addEventListener('click', btnClicked);
 
 function btnClicked() {
-    let vertxA = document.getElementById('vert-1x').value;
-    let vertyA = document.getElementById('vert-1y').value;
-    let vertxB = document.getElementById('vert-2x').value;
-    let vertyB = document.getElementById('vert-2y').value;
-    let vertxC = document.getElementById('vert-3x').value;
-    let vertyC = document.getElementById('vert-3y').value;
-
-    // document.getElementById('perimeterTotal').innerHTML = dist(vertxA, vertxB, vertyB, vertyC)
-    document.getElementById('cLength').innerHTML = cData(vertyA, vertyB);
+    let vertxA = +document.getElementById('vert-1x').value;
+    let vertyA = +document.getElementById('vert-1y').value;
+    let vertxB = +document.getElementById('vert-2x').value;
+    let vertyB = +document.getElementById('vert-2y').value;
+    let vertxC = +document.getElementById('vert-3x').value;
+    let vertyC = +document.getElementById('vert-3y').value;
+    
+    // retrieves function
+    let aLength = document.getElementById('aLength').innerHTML = dist(vertyB, vertyC, vertxB, vertxC);
+    let cLength = document.getElementById('cLength').innerHTML = dist(vertyA, vertyB, vertxA, vertxB);
+    let bLength = document.getElementById('bLength').innerHTML = dist(vertyC, vertyA, vertxC, vertyA);
+    document.getElementById('perimeterTotal').innerHTML = peri(aLength, cLength, bLength);
 }
 
-// function dist(x1Val, y1Val, x2Val, y2Val) {
-//     // let abValue = vertxA - vertxB;
-//     // let bcValue = vertyB - vertyC;
-//     let aValue = x1Val - x2Val;
-//     let cValue = y1Val - y2Val;
-
-//     let bValue = Math.sqrt(aValue**2 + bValue**2);
-
-//     return bValue;
-// }
-
-function cData(y1Val, y2Val) {
-    document.getElementById('cLength').value;
-    let cDisp = y1Val - y2Val;
-    return cDisp;
+// math for distances
+function dist(y1Val, y2Val, x1Val, x2Val) {
+    let xValue = x1Val - x2Val
+    let yValue = y1Val - y2Val
+    let distance = Math.sqrt(xValue ** 2 + yValue ** 2)
+    return distance;
 }
 
-function aData ()
+// gets values to add
+function peri(aValue, cValue, bValue) {
+    let perimeter = aValue + bValue + cValue;
+    return perimeter;
+}
