15ed28bb53aff7b6d6cc053e2e15879981efc4b2
Loull62 <112659573+Loull62@users.noreply.github.com>
2022-09-12 09:56:16 -0600

    whasgd

--- main.js
+++ main.js
@@ -3,24 +3,32 @@
 document.getElementById('btn').addEventListener('click', btnClicked);
 
 function btnClicked() {
-    let vertxA = +document.getElementById('vert-1x').value;
-    let vertyA = +document.getElementById('vert-1y').value;
-    let vertxB = +document.getElementById('vert-2x').value;
-    let vertyB = +document.getElementById('vert-2y').value;
-    let vertxC = +document.getElementById('vert-3x').value;
-    let vertyC = +document.getElementById('vert-3y').value;
+    let vertxA = document.getElementById('vert-1x').value;
+    let vertyA = document.getElementById('vert-1y').value;
+    let vertxB = document.getElementById('vert-2x').value;
+    let vertyB = document.getElementById('vert-2y').value;
+    let vertxC = document.getElementById('vert-3x').value;
+    let vertyC = document.getElementById('vert-3y').value;
 
-    document.getElementById('abTotal').innerHTML = dist(vertxA, vertxB, vertyB, vertyC)
+    // document.getElementById('perimeterTotal').innerHTML = dist(vertxA, vertxB, vertyB, vertyC)
+    document.getElementById('cLength').innerHTML = cData(vertyA, vertyB);
 }
 
-function dist(x1Val, y1Val, x2Val, y2Val) {
-    // let abValue = vertxA - vertxB;
-    // let bcValue = vertyB - vertyC;
-    let aValue = x1Val - x2Val;
-    let cValue = y1Val - y2Val;
+// function dist(x1Val, y1Val, x2Val, y2Val) {
+//     // let abValue = vertxA - vertxB;
+//     // let bcValue = vertyB - vertyC;
+//     let aValue = x1Val - x2Val;
+//     let cValue = y1Val - y2Val;
 
-    let bValue = mathsqrt(aValue**2 + bValue**2);
+//     let bValue = Math.sqrt(aValue**2 + bValue**2);
 
+//     return bValue;
+// }
 
-    return bValue;
+function cData(y1Val, y2Val) {
+    document.getElementById('cLength').value;
+    let cDisp = y1Val - y2Val;
+    return cDisp;
 }
+
+function aData ()
